b39c014d1637bb4f27604258d7b2bf2271d6daff
Anthony Legg <leggtc1@student.op.ac.nz>
2023-11-13 16:44:18 +1300

    init project

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  poweredByHeader: false,
+  trailingSlash: false,
+};
+
+module.exports = nextConfig
