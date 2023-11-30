0ff76831dd77826685a2fdab1a5b7b71a30f4004
Anthony Legg <leggtc1@student.op.ac.nz>
2023-11-30 13:44:05 +1300

    update eslint config

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const ProductsPage: NextPage = () => {
+  return (
+    <div>
+      <h2>Products Page</h2>
+    </div>
+  );
+};
+
+export default ProductsPage;
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const UsersPage: NextPage = () => {
+  return (
+    <div>
+      <h2>Users Page</h2>
+    </div>
+  );
+};
+
+export default UsersPage;
